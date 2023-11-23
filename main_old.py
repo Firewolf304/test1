3a340fba1092743ad337987a86a4f03e8cfe5432
Firewolf304 <nik.mark.bkc@mail.ru>
2023-11-23 17:45:07 +0700

    finish_1

---
+++ main_old.py
@@ -0,0 +1,147 @@
+from typing import List, Any
+import copy
+#import threading
+
+mass = []
+from src import errors
+
+n = int(input("Enter y size: "))
+m = int(input("Enter x size: "))
+for i in range(n):
+    text = list(map(int,input("Enter the numbers : ").strip().split()))[:m]
+    mass.append(text)
+format = list(map(int,input("Enter format : ").strip().split()))[:m]
+equality = list(map(int,input("Enter equality : ").strip().split()))[:m]
+based = []
+notbased = []
+if(len(mass[0]) != len(format)):
+    raise errors.InputError("Error input, this is size: " + str(len(mass[0])))
+
+
+def check(y:int, x: int) -> bool:
+    global mass
+    for Y in range(n):
+        if (Y!=y and mass[Y][x] != 0):
+            return False
+    return True
+
+for y in range(n):
+    for x in range(m):
+        if(mass[y][x] == 1 and check(y,x) and len(based) == y):
+            #based.insert(0,x)
+            based.append(x)
+        if( len(based) == y or (mass[y][x] != 1 and not(check(y,x))) ):
+            notbased.append(x)
+
+notbased = list(set( [x for x in notbased if x not in based] ))
+#print("mass",mass)
+#print("based =",based, "not based =", notbased)
+
+itermatrix: list[Any] = []
+#
+for y in range(n):  # make start matrix
+    itermatrix.append(list([]))
+    for x in range(m): # use Python methods!
+        if(not(x in based)):
+            itermatrix[y].append(mass[y][x])
+            #summ[y] += mass[y][x] * format[based[y]]
+    itermatrix[y].append(equality[y])
+#print("itermatrix =", itermatrix)
+
+def summ() -> list[Any]:
+    summ = [0]*(len(notbased)+1)   # for F in end of matrix
+    for x in range(len(summ)):
+        for y in range(len(itermatrix)):
+            summ[x] += itermatrix[y][x] * format[based[y]]
+            #print(summ[x])
+    return summ
+
+
+#for i in notbased:
+itermatrix.append( list(map(lambda a,b: a-b, summ(),  [format[x] for x in notbased] + [0] ) ) )
+#map(lambda a,b: a-b, summ, format[based[y]] )
+print("start = " + str(itermatrix))
+
+
+old = itermatrix
+new = [[ -1 for _ in range(len(itermatrix[0]))] for _ in range(len(itermatrix))]
+
+#new = [[-1]*len(itermatrix[0])] * len(itermatrix)
+
+
+
+def changeBest():
+    global bestIndexX, bestIndexY, new
+    nig_mass = list(map(lambda a: abs(a), [x for x in old[-1][:-1] if x < 0]))  # all abs of negative values
+    value = nig_mass.count(max(nig_mass))
+    if (value > 1):  # if some values in the F
+        listing: list[dict] = []
+
+        def func(index: int) -> None:
+            global listing
+            bestIndY = 0
+            val = 9999999
+            for i in range(len(based)):
+                try:
+                    if (val >= old[i][len(notbased)] / old[i][index] and old[i][index] > 0):
+                        val = old[i][len(notbased)] / old[i][index]
+                        bestIndY = i
+                except Exception:
+                    pass
+            listing.append(dict({'X': index, 'Y': bestIndY, 'val': val}))
+
+        # maximus = map( lambda a: a==max(nig_mass) ,  )
+        save = 0
+        for i in range(value):  # use python methods!
+            save = nig_mass.index(max(nig_mass), save)
+            func(save)
+        element = min(listing, key=lambda x: x['val'])
+        bestIndexX = element['X']
+        bestIndexX = element['Y']
+        pass
+    else:
+        bestIndexX = old[-1].index(max(nig_mass) * -1)
+        val = 9999999
+        for i in range(len(based)):
+            try:
+                if (val >= old[i][len(notbased)] / old[i][bestIndexX] and old[i][bestIndexX] > 0):
+                    val = old[i][len(notbased)] / old[i][bestIndexX]
+                    bestIndexY = i
+            except Exception:
+                pass
+    print('best coords (x y):', bestIndexX, bestIndexY)
+
+test = 0
+### ========================================ITERATION========================================
+while(not(all(map(lambda x: x >= 0, new[-1][:-1])))):
+#while(test < 2):
+    #maximus = max( abs(list(map(lambda a: a <0 , old[-1]))) )
+    new = [[ 0 for _ in range(len(itermatrix[0]))] for _ in range(len(itermatrix))]
+    maxInd = 0
+    val = 0
+    bestIndexX = 0
+    bestIndexY = 0
+
+    changeBest()
+    new[bestIndexY][bestIndexX] = float(1 / old[bestIndexY][bestIndexX])
+
+    # заебали эти вумные методы, я в тупую
+    for y in range(len(new)):
+        if y != bestIndexY:
+            new[y][bestIndexX] = old[y][bestIndexX] * new[bestIndexY][bestIndexX] * (-1)
+    for x in range(len(notbased)+1):
+        if x != bestIndexX:
+            new[bestIndexY][x] = float(old[bestIndexY][x] * new[bestIndexY][bestIndexX])
+
+    for y in range(len(new)):
+        for x in range(len(new[y])):
+            if(y != bestIndexY and x != bestIndexX):
+                new[y][x] = (old[y][x] * old[bestIndexY][bestIndexX] - old[bestIndexY][x] * old[y][bestIndexX] ) / old[bestIndexY][bestIndexX]
+
+    old = copy.copy(new)
+    oldbestIndexX = bestIndexX
+    oldbestIndexY = bestIndexY
+    print(new)
+    #test +=1
+    pass
+
--- src/constructor.py
+++ src/constructor.py
@@ -1,2 +1,153 @@
-class MatrixCnstructor:
+class InputError(Exception):
     pass
+import copy
+from typing import List, Any
+class MatrixCnstructor:
+    def __init__(self, n:int, m:int, mass:list[list], format:list[Any], equality: list[Any]):
+        self.based = []
+        self.notbased = []
+        self.format = format
+        self.n = n
+        self.m = m
+        self.mass = mass
+        self.equality = equality
+        self.itermatrix: list[Any] = []
+        self.old = None
+        self.new = None
+        self.bestIndexX = 0
+        self.bestIndexY = 0
+        if (len(mass[0]) != len(self.format)):
+            raise InputError("Error input, this is size: " + str(len(mass[0])))
+        pass
+    def check(self, y:int, x:int) -> bool:
+        """
+        Проверка столбца на наличие единичной матрицы.
+        Если есть что-то не 0 (не трогая строку (x,y)), то сброс
+        :param y: рассмотр координаты относительно Y
+        :param x: рассмотр координаты относительно X
+        :return: успешность нахождения нулевого столбца
+        """
+        for Y in range(self.n):
+            if (Y != y and self.mass[Y][x] != 0):
+                return False
+        return True
+    def make_based(self):
+        """
+        Заполнение based и notbased переменных\n
+        based - базисные переменные, что слева
+        notbased - не базисные переменные, что сверху
+        :return: None
+        """
+        for y in range(self.n):
+            for x in range(self.m):
+                if (self.mass[y][x] == 1 and self.check(y, x) and len(self.based) == y):
+                    # based.insert(0,x)
+                    self.based.append(x)
+                if (len(self.based) == y or (self.mass[y][x] != 1 and not (self.check(y, x)))):
+                    self.notbased.append(x)
+        self.notbased = list(set([x for x in self.notbased if x not in self.based]))
+    def makeMatrix(self):
+        """
+        Заполнение основной матрицы itermatrix (до make_based) \n
+        А также заполнение new и old таблиц
+        :return: None
+        """
+        for y in range(self.n):  # make start matrix
+            self.itermatrix.append(list([]))
+            for x in range(self.m):  # use Python methods!
+                if (not (x in self.based)):
+                    self.itermatrix[y].append(self.mass[y][x])
+                    # summ[y] += mass[y][x] * format[based[y]]
+            self.itermatrix[y].append(self.equality[y])
+        self.itermatrix.append(list(map(lambda a, b: a - b, self.summ(), [self.format[x] for x in self.notbased] + [0])))
+        # map(lambda a,b: a-b, summ, format[based[y]] )
+        print("start = " + str(self.itermatrix))
+        self.old = self.itermatrix
+        self.new = [[ -1 for _ in range(len(self.itermatrix[0]))] for _ in range(len(self.itermatrix))]
+
+    def summ(self) -> list[Any]:
+        """
+        Ищет сумму всех столбцов по формуле:          \n
+        summ[x]+=itermatrix[y][x]*format[based[y]]    \n
+        сумма      матрица        максимизация
+        :return:
+        """
+        summ = [0] * (len(self.notbased) + 1)  # for F in end of matrix
+        for x in range(len(summ)):
+            for y in range(len(self.itermatrix)):
+                summ[x] += self.itermatrix[y][x] * self.format[self.based[y]]
+                # print(summ[x])
+        return summ
+
+    def changeBest(self):
+        """
+        Вычисление главных столбцов
+        :return:
+        """
+        nig_mass = list(map(lambda a: abs(a), [x for x in self.old[-1][:-1] if x < 0]))  # all abs of negative values
+        value = nig_mass.count(max(nig_mass))
+        if (value > 1):  # if some values in the F
+            listing: list[dict] = []
+
+            def func(index: int) -> None:
+                global listing
+                bestIndY = 0
+                val = 9999999
+                for i in range(len(self.based)):
+                    try:
+                        if (val >= self.old[i][len(self.notbased)] / self.old[i][index] and self.old[i][index] > 0):
+                            val = self.old[i][len(self.notbased)] / self.old[i][index]
+                            bestIndY = i
+                    except Exception:
+                        pass
+                listing.append(dict({'X': index, 'Y': bestIndY, 'val': val}))
+
+            # maximus = map( lambda a: a==max(nig_mass) ,  )
+            save = 0
+            for i in range(value):  # use python methods!
+                save = nig_mass.index(max(nig_mass), save)
+                func(save)
+            element = min(listing, key=lambda x: x['val'])
+            self.bestIndexX = element['X']
+            self.bestIndexX = element['Y']
+            pass
+        else:
+            self.bestIndexX = self.old[-1].index(max(nig_mass) * -1)
+            val = 9999999
+            for i in range(len(self.based)):
+                try:
+                    if (val >= self.old[i][len(self.notbased)] / self.old[i][self.bestIndexX] and self.old[i][self.bestIndexX] > 0):
+                        val = self.old[i][len(self.notbased)] / self.old[i][self.bestIndexX]
+                        self.bestIndexY = i
+                except Exception:
+                    pass
+        print('best coords (x y):', self.bestIndexX, self.bestIndexY)
+    def iterator(self):
+        while (not (all(map(lambda x: x >= 0, self.new[-1][:-1])))):
+            self.new = [[0 for _ in range(len(self.itermatrix[0]))] for _ in range(len(self.itermatrix))]
+            maxInd = 0
+            val = 0
+            self.bestIndexX = 0
+            self.bestIndexY = 0
+
+            self.changeBest()
+            self.new[self.bestIndexY][self.bestIndexX] = float(1 / self.old[self.bestIndexY][self.bestIndexX])
+
+            # заебали эти вумные методы, я в тупую
+            for y in range(len(self.new)):
+                if y != self.bestIndexY:
+                    self.new[y][self.bestIndexX] = self.old[y][self.bestIndexX] * self.new[self.bestIndexY][self.bestIndexX] * (-1)
+            for x in range(len(self.notbased) + 1):
+                if x != self.bestIndexX:
+                    self.new[self.bestIndexY][x] = float(self.old[self.bestIndexY][x] * self.new[self.bestIndexY][self.bestIndexX])
+
+            for y in range(len(self.new)):
+                for x in range(len(self.new[y])):
+                    if (y != self.bestIndexY and x != self.bestIndexX):
+                        self.new[y][x] = (self.old[y][x] * self.old[self.bestIndexY][self.bestIndexX] - self.old[self.bestIndexY][x] * self.old[y][
+                            self.bestIndexX]) / self.old[self.bestIndexY][self.bestIndexX]
+
+            self.old = copy.copy(self.new)
+            print(self.new)
+            # test +=1
+            pass
