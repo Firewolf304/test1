f1e532790dd6348e724b0ec60cdf80b5af922586
Firewolf304 <nik.mark.bkc@mail.ru>
2023-11-23 16:06:50 +0700

    there was a stroke left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from typing import List, Any
+import copy
+#import threading
 
 mass = []
 from src import errors
@@ -8,16 +10,22 @@ m = int(input("Enter x size: "))
 for i in range(n):
     text = list(map(int,input("Enter the numbers : ").strip().split()))[:m]
     mass.append(text)"""
+n = 2
+m = 4
+mass = [[0,3,1,1],[1,-3,-2,0]]
+format = [3,4,3,0]
+equality = [5,7]
+#n = 3
+#m = 6
+#mass = [[1,1,1,0,0,0],[0,2,0,0,1,1],[0,2,1,1,0,0]]
+#format = [-2,-1,0,0,0,0]
+#equality = [2,5,1]
 #n = 2
 #m = 4
 #mass = [[0,3,1,1],[1,-3,-2,0]]
-#format = [3,4,3,0]
+#format = [3,0,3,0]
 #equality = [5,7]
-n = 3
-m = 6
-mass = [[1,1,1,0,0,0],[0,2,0,0,1,1],[0,2,1,1,0,0]]
-format = [-2,-1,0,0,0,0]
-equality = [2,5,1]
+
 based = []
 notbased = []
 if(len(mass[0]) != len(format)):
@@ -70,24 +78,84 @@ print("start = " + str(itermatrix))
 
 
 old = itermatrix
-new = [[-1]*len(itermatrix[0])] * len(itermatrix)
-### ITERATION
-while(not(all(map(lambda x: x >= 0, new[-1])))):
+new = [[ -1 for _ in range(len(itermatrix[0]))] for _ in range(len(itermatrix))]
+
+#new = [[-1]*len(itermatrix[0])] * len(itermatrix)
+
+
+
+def changeBest():
+    global bestIndexX, bestIndexY, new
+    nig_mass = list(map(lambda a: abs(a), [x for x in old[-1][:-1] if x < 0]))  # all abs of negative values
+    value = nig_mass.count(max(nig_mass))
+    if (value > 1):  # if some values in the F
+        listing: list[dict] = []
+
+        def func(index: int) -> None:
+            global listing
+            bestIndY = 0
+            val = 9999999
+            for i in range(len(based)):
+                try:
+                    if (val >= old[i][len(notbased)] / old[i][index] and old[i][index] > 0):
+                        val = old[i][len(notbased)] / old[i][index]
+                        bestIndY = i
+                except Exception:
+                    pass
+            listing.append(dict({'X': index, 'Y': bestIndY, 'val': val}))
+
+        # maximus = map( lambda a: a==max(nig_mass) ,  )
+        save = 0
+        for i in range(value):  # use python methods!
+            save = nig_mass.index(max(nig_mass), save)
+            func(save)
+        element = min(listing, key=lambda x: x['val'])
+        bestIndexX = element['X']
+        bestIndexX = element['Y']
+        pass
+    else:
+        bestIndexX = old[-1].index(max(nig_mass) * -1)
+        val = 9999999
+        for i in range(len(based)):
+            try:
+                if (val >= old[i][len(notbased)] / old[i][bestIndexX] and old[i][bestIndexX] > 0):
+                    val = old[i][len(notbased)] / old[i][bestIndexX]
+                    bestIndexY = i
+            except Exception:
+                pass
+    print('best coords (x y):', bestIndexX, bestIndexY)
+
+test = 0
+### ========================================ITERATION========================================
+#while(not(all(map(lambda x: x >= 0, new[-1])))):
+while(test < 2):
     #maximus = max( abs(list(map(lambda a: a <0 , old[-1]))) )
-    new = [[0]*len(itermatrix[0])] * len(itermatrix)
+    new = [[ 0 for _ in range(len(itermatrix[0]))] for _ in range(len(itermatrix))]
     maxInd = 0
     val = 0
-    recursive_check = False                         # if some values in the F
-    for i in range(len(notbased)):
-        if old[-1][i] < 0 and abs(old[-1][i]) > val:
-            val = abs(old[-1][i])
-            maxInd = i
-        elif
-    nextCheck = []
-    for y in range(len(based)):
-        nextCheck.append( old[y][len(notbased)]/old[y][maxInd])
+    bestIndexX = 0
+    bestIndexY = 0
 
+    changeBest()
+    new[bestIndexY][bestIndexX] = float(1 / old[bestIndexY][bestIndexX])
 
-    #itermatrix[-1].index(maximus*(-1))
+    # заебали эти вумные методы, я в тупую
+    for y in range(len(new)):
+        if y != bestIndexY:
+            new[y][bestIndexX] = old[y][bestIndexX] * new[bestIndexY][bestIndexX] * (-1)
+    for x in range(len(notbased)+1):
+        if x != bestIndexX:
+            new[bestIndexY][x] = float(old[bestIndexY][x] * new[bestIndexY][bestIndexX])
+
+    for y in range(len(new)):
+        for x in range(len(new[y])):
+            if(y != bestIndexY and x != bestIndexX):
+                new[y][x] = (old[y][x] * old[bestIndexY][bestIndexX] - old[0][x] * old[y][0] ) / old[bestIndexY][bestIndexX]
+
+    old = copy.copy(new)
+    oldbestIndexX = bestIndexX
+    oldbestIndexY = bestIndexY
+    print(new)
+    test +=1
     pass
 
